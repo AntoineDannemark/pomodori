d270963c85e6842b3ab777046a7c49401083f258
AntoineDannemark <antoine.dannemark@gmail.com>
2019-02-03 16:21:02 +0100

    new branch w/o animation

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -25025,7 +25025,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44353" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42671" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);

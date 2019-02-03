e079bb3261ccf4d50a58ae03ad2278e683350695
AntoineDannemark <antoine.dannemark@gmail.com>
2019-02-03 16:19:20 +0100

    anim@work

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -25131,7 +25131,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43969" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42671" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);

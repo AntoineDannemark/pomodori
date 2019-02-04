f73bc31c14ab6e632ff3bd08e4ae014b694a1488
AntoineDannemark <antoine.dannemark@gmail.com>
2019-02-04 09:41:01 +0100

    remove unnecessary props(next-prev digits - anim @timer.js)

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,29 +3,16 @@ import TwoDigits from './TwoDigits';
 import Separator from './Separator';
 import './Timer.css';
 
-const Timer = ({digits, prevVals, nextVals, anims}) => (
+const Timer = ({digits}) => (
     <div className="timer">
             <TwoDigits 
                 digitOne={digits[0]} 
                 digitTwo={digits[1]}
-                prevValOne={prevVals[0]}
-                prevValTwo={prevVals[1]}
-                nextValOne={nextVals[0]}
-                nextValTwo={nextVals[1]}
-                animOne={anims[0]}
-                animTwo={anims[1]}
-
             />         
             <Separator />
             <TwoDigits 
                 digitOne={digits[2]} 
                 digitTwo={digits[3]}
-                prevValOne={prevVals[2]}
-                prevValTwo={prevVals[3]}
-                nextValOne={nextVals[2]}
-                nextValTwo={nextVals[3]}
-                animOne={anims[2]}
-                animTwo={anims[3]}
             />    
     </div>
 )

51a557fa662be2d3cfff070c48f60544a96608f4
AntoineDannemark <antoine.dannemark@gmail.com>
2019-02-04 09:55:35 +0100

    test dploy

--- src/components/Digit.js
+++ src/components/Digit.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './Digit.css';
 
-const Digit = ({curVal, prevVal, nextVal, anim}) => (
-    <div className={`threeDigitsWrapper ${anim}`}>
-        <div className="digitWrapper">
-            <div className="digit">{prevVal}</div>
-        </div>
+const Digit = ({curVal}) => (
         <div className="digitWrapper">
             <div className="digit">{curVal}</div>
         </div>
-        <div className="digitWrapper">
-            <div className="digit">{nextVal}</div>
-        </div>
-    </div>
 ) 
 
 export default Digit;
